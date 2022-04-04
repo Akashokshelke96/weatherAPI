b6ddc8d419134427d21315d71d8d0ecc0a2fc765
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-05 00:05:35 +0530

    initial commit

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -10,7 +10,14 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+		System.out.println("inspecting the beans provided by the spring boot");
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for(String beanName : beanNames){
+			System.out.println(beanName);
+		}
 
 
 	}
