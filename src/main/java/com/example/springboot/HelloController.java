48fb3332ea3aca19151ec770a9883c26425df340
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-04 22:33:26 +0530

    initial commit

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.springboot;
 
+import com.example.springboot.service.WeatherService;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class HelloController {
+	@Autowired
+	private WeatherService weatherService;
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot! WE SHOULD START LEARNING";
+	@GetMapping("/weather/{city}/{noOfDays}")
+	public String index(@PathVariable("city") String city , @PathVariable("noOfDays") Integer noOfDays) {
+		return  weatherService.getWeather(city,noOfDays);
 	}
 
 	@GetMapping("/hello")
