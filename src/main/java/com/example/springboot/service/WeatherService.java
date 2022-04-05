0a788d46c781c5f9cdfaff3a40d7032e2a67c9cd
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-05 21:38:17 +0530

    initial commit

--- src/main/java/com/example/springboot/service/WeatherService.java
+++ src/main/java/com/example/springboot/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.example.springboot.service;
 
 
 import com.example.springboot.accessor.WeatherAccessor;
+import com.example.springboot.exceptions.InvalidCityException;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -18,16 +19,17 @@ public class WeatherService {
 
     private WeatherAccessor weatherAccessor;
 
-    public String getWeather(String city, Integer noOfDays) {
-        try {
+    public HttpResponse<String> getWeather(String city, Integer noOfDays) throws InvalidCityException, UnirestException {
+        if(city.length() <= 2){
+            throw new InvalidCityException(city) ;
+        }
             HttpResponse<String> response = Unirest.get("https://weatherapi-com.p.rapidapi.com/forecast.json?q=" + city + "&days=" + noOfDays)
                     .header("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
                     .header("X-RapidAPI-Key", "8a6a4710a7msh85d1b8bd632cb36p1f1f29jsn594aa65e5195")
                     .asString();
             weatherAccessor.someMethod();
-            return response.getBody();
-        } catch (UnirestException e) {
-            return e.getMessage();
+            return response;
         }
-    }
-}
+        }
+
+
