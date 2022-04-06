23902b6cc2c0317e1e86b420e0ff721cac90d184
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-06 13:09:46 +0530

    initial commit

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.springboot;
 
 import com.example.springboot.exceptions.InvalidCityException;
+import com.example.springboot.response.model.Weather;
 import com.example.springboot.service.WeatherService;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -20,7 +21,10 @@ public class HelloController {
     private WeatherService weatherService;
 
     @GetMapping("/weather/{city}/{noOfDays}")
-    public ResponseEntity<String> index(@PathVariable("city") String city, @PathVariable("noOfDays") Integer noOfDays) {
+   // public ResponseEntity<String> index(@PathVariable("city") String city, @PathVariable("noOfDays") Integer noOfDays) {
+        public ResponseEntity<Weather> index(@PathVariable("city") String city, @PathVariable("noOfDays") Integer noOfDays) { //<weather> here is used while sending JSON
+                                                                  //now the body will be just CITY-NAME & REGION-NAME FROM THE RECEIVED json.
+
         try {
             JSONParser jsonParser = new JSONParser(); //creating a json parser object
 
@@ -37,23 +41,28 @@ public class HelloController {
 
 
             JSONObject currentObj = (JSONObject) jsonObject.get("current"); // here we have put "current" from json to currentObj
-            double temp_c = (Double) currentObj.get("temp_c");
+            double temp_c = (Double) currentObj.get("temp_c");//temp_c is given in the JSON received so here pe put it under temp_c double variable.
 
             System.out.println(" Name = " + name + " Region = " + region + " Temp_C " +temp_c);
+
+            Weather weather = new Weather(city,region);
 
 
 
 //            HttpHeaders headers = new HttpHeaders();//next two lines we have added another header to the response code.
 //            headers.add("Name", "Akash");
 
-            return ResponseEntity.status(unirestResponse.getStatus()).body(result);
+            return ResponseEntity.status(unirestResponse.getStatus()).body(weather); //earlier instead of weather result was used with @getmapping string
         } catch (InvalidCityException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+          //  return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 
         } catch (UnirestException ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+          //  return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage()); //this one was used with the String
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         } catch (ParseException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+          //  return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 
         }
     }
